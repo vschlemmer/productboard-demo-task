2964d74851366fce9c6be3e174a4089ab6bb81c1
Vojtěch Schlemmer <vojtech.schlemmer@morosystems.cz>
2022-09-05 21:23:10 +0200

    added database and liquibase

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,10 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.liquibase:liquibase-core")
+
+    runtimeOnly("org.postgresql:postgresql")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
---
+++ src/main/resources/db/migrations/0001-language-percentage.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+
+--changeset vojtech.schlemmer:1
+CREATE TABLE language_percentage
+(
+    id            BIGSERIAL PRIMARY KEY,
+    language_name TEXT UNIQUE NOT NULL,
+    percentage    DECIMAL(2, 1)
+);
