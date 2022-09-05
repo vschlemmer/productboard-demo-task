b5029921344007d25dd8eac126416dd37a54e8e2
Vojtěch Schlemmer <vojtech.schlemmer@morosystems.cz>
2022-09-05 20:49:12 +0200

    spring boot skeleton

---
+++ src/main/kotlin/cz/productboard/hire/schlemmer/languagepercentage/LanguagePercentageApplication.kt
@@ -0,0 +1,11 @@
+package cz.productboard.hire.schlemmer.languagepercentage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LanguagePercentageApplication
+
+fun main(args: Array<String>) {
+	runApplication<LanguagePercentageApplication>(*args)
+}
---
+++ src/test/kotlin/cz/productboard/hire/schlemmer/languagepercentage/LanguagePercentageApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.productboard.hire.schlemmer.languagepercentage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LanguagePercentageApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
